474de15df5ed46a11b2c0be2525510a42e93b968
Hyerim Han <105032106+hyeeee14@users.noreply.github.com>
2025-02-13 16:13:53 +0000

    ConversationSummaryBufferMemory config 추가

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import os, json, chardet, openai
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain_core.tools import tool
+from langchain_community import ConversationSummaryBufferMemory
 
 # streamlit secrets
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
